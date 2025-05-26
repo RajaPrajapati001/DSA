32278d54075ebf0a93b66fe8f1ed14e576f4c45a
RajaPrajapati001 <rajaprajapati3454@gmail.com>
2025-05-26 13:23:51 +0530

    Update ValidAnagram.java

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -1,29 +1,26 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         TreeMap<String, Integer> arr1 = new TreeMap<>();
-        for(int i=0;i<s.length();i++){
-            String letter=Character.toString(s.charAt(i));
-            if(arr1.containsKey(letter)){
-                int value=arr1.get(letter);
-                arr1.put(letter,value+1);
-            }else{
-                arr1.put(letter,1);
-            }
-        }
         TreeMap<String, Integer> arr2 = new TreeMap<>();
-        for(int i=0;i<t.length();i++){
-            String letter=Character.toString(t.charAt(i));
-            if(arr2.containsKey(letter)){
-                int value=arr2.get(letter);
-                arr2.put(letter,value+1);
-            }else{
-                arr2.put(letter,1);
-            }
-        }
+        arr1=loop(s);
+        arr2=loop(t);
         if (arr1.equals(arr2)) {
             return true;
         }else{
             return false;
         }
+    }
+    public static TreeMap loop(String name){
+        TreeMap<String, Integer> arr = new TreeMap<>();
+        for(int i=0;i<name.length();i++){
+            String letter=Character.toString(name.charAt(i));
+            if(arr.containsKey(letter)){
+                int value=arr.get(letter);
+                arr.put(letter,value+1);
+            }else{
+                arr.put(letter,1);
+            }
+        }
+        return arr;
     }
 }
